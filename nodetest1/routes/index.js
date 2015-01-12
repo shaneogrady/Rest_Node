e19d56b3cbfc16f52a8eecba0588317d9e066588
Shane O'Grady <shaneogrady1991@gmail.com>
2015-01-12 22:03:13 +0000

    Initial Setup, View is reading data from mongo database

---
+++ nodetest1/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' })
+});
+
+module.exports = router;
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
